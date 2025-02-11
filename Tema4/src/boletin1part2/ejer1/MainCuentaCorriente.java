92b0b76fb0c763cf94cce866a8013eaf422f219c
ruben-carrasco <ruben.carrasco@iesnervion.es>
2025-02-11 14:37:04 +0100

    Ejer1 par2

---
+++ Tema4/src/boletin1part2/ejer1/MainCuentaCorriente.java
@@ -0,0 +1,20 @@
+package boletin1part2.ejer1;
+
+public class MainCuentaCorriente {
+	public static void main(String[] args) {
+		// Creo dos cuentas
+		CuentaCorriente c1 = new CuentaCorriente("52077975H","Ruben",20000,"ESPAÑOLA");
+		CuentaCorriente c2 = new CuentaCorriente("52077975H","Ruben",20000,"ESPAÑOLA");
+		
+		// Las muestro
+		System.out.println(c1);
+		
+		// Quito saldo
+		c1.sacarDinero(10000);
+		System.out.println(c1.getSaldo());
+		
+		// Añado saldo 
+		s1.a
+	}
+
+}
